98ef4fb8d1f73ab73d8c7d738193570386ca0c87
sparshgupta1705 <sparshgupta1705@gmail.com>
2026-07-07 20:07:12 +0530

    Connect Express server to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import connectDB from "./config/db.js";
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -16,5 +21,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
